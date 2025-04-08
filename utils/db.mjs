6177f9f99d57ef33a3f5f39b6d419add3b1797ff
NicoBoni55 <bonifrank55@gmail.com>
2025-04-08 14:02:50 -0300

    fix version

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,39 +1,44 @@
 import { MongoClient } from 'mongodb';
+import { env } from 'process';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const DB_HOST = env.DB_HOST || 'localhost';
+const DB_PORT = env.DB_PORT || 27017;
+const DB_DATABASE = env.DB_DATABASE || 'files_manager';
 const URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-class DBClient{
-    constructor(){
-        this.connection = false
-        const options = {
-            useUnifiedTopology: true,
-        }
+class DBClient {
+  constructor() {
+    this.connection = false;
+    const connectOptions = {
+      useUnifiedTopology: true,
+    };
 
-        MongoClient.connect(URI, options, (error, client) => {
-            if(error) {
-                console.error(`Error to connect database`)
-            }
-            this.connection = true;
-            this.db = client.db()
-            this.users = this.db.collection('users')
-            this.files = this.db.collection('files')
-        });
-    }
+    MongoClient.connect(URI, connectOptions, (error, client) => {
+      if (error) {
+        console.error(error.message);
+      }
+      this.connection = true;
+      this.db = client.db();
+      this.users = this.db.collection('users');
+      this.files = this.db.collection('files');
+    });
+  }
 
-    isAlive() {
-        return this.connection;
-    }
+  isAlive() {
+    return this.connection;
+  }
 
-    async nbUsers() {
-        return await this.users.countDocuments();
-    }
-    async nbFiles() {
-        return await this.files.countDocuments()
-    }
+  async nbUsers() {
+    const userCount = await this.users.countDocuments();
+    return userCount;
+  }
+
+  async nbFiles() {
+    const fileCount = await this.files.countDocuments();
+    return fileCount;
+  }
 }
 
 const dbClient = new DBClient();
+
 export default dbClient;
