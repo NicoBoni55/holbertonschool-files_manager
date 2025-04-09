571c6fc5af88f368526a982509b8f5e84d5b1c7f
NicoBoni55 <bonifrank55@gmail.com>
2025-04-09 17:53:43 -0300

    fist Api

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+export default app;
